2b57111c6cc96cd03a4490d9c3b53e289d78b27a
Alexander Obregon <alexander.obregon47@gmail.com>
2022-09-29 14:37:07 -0700

    List ready

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
-const LinkedList = require('./LinkedList')
+class LinkedList {
+    constructor() {
+        this.head = null
+        this.length = 0
+    }
+    insertAtHead(data) {
+        const newNode = new LinkedListNode(data, this.head)
+        this.head = newNode
+        this.length++
+    }
+
+}
+
+class LinkedListNode {
+    constructor(value, next) {
+    this.value = value
+    this.next = next
+    }
+}
+
 
 const ll = new LinkedList()
 ll.insertAtHead(10)
